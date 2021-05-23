aadee606ef2f0aca600966921f346a2e4a46942c
Eduardo Corrêa <eduardo.correa@cotesa.com.br>
2021-05-22 21:55:09 -0300

    Finalização do projeto

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Visitor;
+use App\Page;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -12,7 +15,52 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    public function index() {
-        return view('admin.home');
+    public function index(Request $request) {
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pageCount = 0;
+        $userCount = 0;
+        $interval = intval($request->input('interval', 30));
+        if($interval > 120) {
+            $interval = 120;
+        }
+
+        // Contagem de Visitantes
+        $dateInterval = date('Y-m-d H:i:s', strtotime('-'.$interval.' days'));
+        $visitsCount = Visitor::where('date_access', '>=', $dateInterval)->count();
+
+        // Contagem de Usuários Online
+        $datelimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $onlineList = Visitor::select('ip')->where('date_access', '>=', $datelimit)->groupBy('ip')->get();
+        $onlineCount = count($onlineList);
+
+        // Contagem de Páginas
+        $pageCount = Page::count();
+
+        // Contagem de Usuários
+        $userCount = User::count();
+
+        // Contagem para o PagePie
+        $pagePie = [];
+        $visitsAll = Visitor::selectRaw('page, count(page) as c')
+            ->where('date_access', '>=', $dateInterval)
+            ->groupBy('page')
+            ->get();
+        foreach ($visitsAll as $visit) {
+            $pagePie[ $visit['page'] ] = intval($visit['c']);
+        }
+
+        $pageLabels = json_encode( array_keys($pagePie) );
+        $pageValues = json_encode( array_values($pagePie) );
+
+        return view('admin.home', [
+          'visitsCount' =>  $visitsCount,
+          'onlineCount' =>  $onlineCount,
+          'pageCount' =>  $pageCount,
+          'userCount' =>  $userCount,
+          'pageLabels' => $pageLabels,
+          'pageValues' => $pageValues,
+          'dateInterval' => $interval
+        ]);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,9 +1,26 @@
 @extends('adminlte::page')
 
+@section('plugins.Chartjs', true)
+
 @section('title', 'Painel')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Dashboard</h1>
+        </div>
+        <div class="col-md-6">
+            <form method="GET">
+                <select onchange="this.form.submit()" name="interval" class="float-md-right">
+                    <option {{$dateInterval==30?'selected="selected"':''}} value="30">Últimos 30 dias</option>
+                    <option {{$dateInterval==60?'selected="selected"':''}} value="60">Últimos 2 meses</option>
+                    <option {{$dateInterval==90?'selected="selected"':''}} value="90">Últimos 3 meses</option>
+                    <option {{$dateInterval==120?'selected="selected"':''}} value="120">Últimos 6 meses</option>
+                </select>
+            </form>
+
+        </div>
+    </div>
 @endsection
 
 @section('content')
@@ -11,8 +28,8 @@
         <div class="col-md-3">
             <div class="small-box bg-info">
                 <div class="inner">
-                    <h3>999</h3>
-                    <p>Visitantes</p>
+                    <h3>{{$visitsCount}}</h3>
+                    <p>Acessos</p>
                 </div>
                 <div class="icon">
                     <i class="far fa-fw fa-eye"></i>
@@ -22,7 +39,7 @@
         <div class="col-md-3">
             <div class="small-box bg-success">
                 <div class="inner">
-                    <h3>999</h3>
+                    <h3>{{$onlineCount}}</h3>
                     <p>Usuarios Online</p>
                 </div>
                 <div class="icon">
@@ -33,7 +50,7 @@
         <div class="col-md-3">
             <div class="small-box bg-warning">
                 <div class="inner">
-                    <h3>999</h3>
+                    <h3>{{$pageCount}}</h3>
                     <p>Páginas</p>
                 </div>
                 <div class="icon">
@@ -44,7 +61,7 @@
         <div class="col-md-3">
             <div class="small-box bg-danger">
                 <div class="inner">
-                    <h3>999</h3>
+                    <h3>{{$userCount}}</h3>
                     <p>Usuários</p>
                 </div>
                 <div class="icon">
@@ -61,7 +78,7 @@
                     <h3 class="card-title">Páginas mais visitadas</h3>
                 </div>
                 <div class="card-body">
-                    ...
+                    <canvas id="pagePie"></canvas>
                 </div>
             </div>
         </div>
@@ -76,6 +93,28 @@
             </div>
         </div>
     </div>
+
+    <script>
+        window.onload = function (){
+            let ctx = document.getElementById('pagePie').getContext('2d');
+            window.pagePie = new Chart(ctx, {
+                type:'pie',
+                data:{
+                    datasets:[{
+                        data: {{$pageValues}},
+                        backgroundColor: '#0000FF'
+                    }],
+                    labels: {!!$pageValues!!}
+                },
+                options:{
+                    resposive:true,
+                    legend:{
+                        display:false
+                    }
+                }
+            });
+        }
+    </script>
 @endsection
 
 
